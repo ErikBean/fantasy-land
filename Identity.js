8d98442dee8ace0c7c5ec7672b97bf7b3cde3abd
Erik Bean <Erik.Bean@cdk.com>
2016-11-15 15:02:01 -0800

    add Identity Monad

---
+++ Identity.js
@@ -0,0 +1,14 @@
+
+function Identity (value) {
+    this.value = value;
+}
+
+Identity.prototype.bind = function(transform) {
+    return transform(this.value);
+};
+
+Identity.prototype.toString = function() {
+    return 'Identity(' + this.value + ')';
+};
+
+module.exports = Identity
---
+++ index.js
@@ -0,0 +1,6 @@
+var Identity = require('./Identity')
+
+var result = new Identity(5)
+
+
+console.log(result.toString())
