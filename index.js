d0ad5eddd71e46b30cd6dd668c9bff0d0e2c7771
Erik Bean <Erik.Bean@cdk.com>
2016-11-15 15:04:58 -0800

    multiple Identities

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var Identity = require('./Identity')
 
-var result = new Identity(5)
+var result = new Identity(5).bind(function(value){
+  return new Identity(6).bind(function(value2){
+    return new Identity(7).bind(function(value3){
+      return new Identity(value + value2 + value3)
+    })
+  })
+})
 
 
 console.log(result.toString())
