87e49e6aa481e706b4cb71df7a98a9dbb925f590
Erik Bean <Erik.Bean@cdk.com>
2016-11-15 17:38:41 -0800

    maybe awesome?

--- index.js
+++ index.js
@@ -1,45 +1,36 @@
 var Identity = require('./Identity')
-var Maybe = require('./Maybe'), Nothing = Maybe.Nothing, Just = Maybe.Just
+// var Maybe = require('./Maybe'), Nothing = Maybe.Nothing, Just = Maybe.Just
 var http = require('./http')
+var sanctuary = require('sanctuary'), create = sanctuary.create, env = sanctuary.env;
 
-
-var result = new Just(5).bind(function(value){
-  return new Just(6).bind(function(value2){
-    return Nothing.bind(function(value3){
-      console.log('Create Maybe with sum of values: ', value , value2 , value3)
-      return new Just(value + value2 + value3)
-    })
-  })
-})
-
-
-console.log('Fantastic Result:', result.toString())
-
-function sum3(a){
-  return function(b){
-    return function(c){
-      console.log(`\t* Nested callback adding: ${a} + ${b} + ${c}`)
-      return a + b + c
-    }
-  }
-}
-console.log('\n\t* But without Maybe construct, execution does not stop on errors!')
-console.log('\nUnfantastic Result: ', sum3(5)()(7), '\n')
-
+const S = create({
+  checkTypes: process.env.NODE_ENV !== 'production',
+  env: env
+});
+const Maybe = S.Maybe 
 
 var desiredColor //want to get cars[0].color
 
-function getColor(data){
-  return new Just(
-    getData: 
-  )
+
+function getCars(data){
+  return data.cars
 }
 
-http.get('bad-foo')
-  .then(getColor)
-  .then(console.log)
+function getFirst(cars){
+  return cars[0]
+}
+
+function getColor(car) {
+  return car.color
+}
+
+http.get('foo')
+  .then(function(data){
+    const maybe = Maybe.of(data)
+    const maybeColor = maybe.map(getCars).map(getFirst).map(getColor)
+    console.log('color: ', maybeColor)
+  })
   .catch((e) => {
-    //so this code is never reached! 
     console.error('.catch() Error handler: ', e)
   })
 
